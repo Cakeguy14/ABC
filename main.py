62d45044b476210f980eac5608750b4c2d78aaae
Cakeguy14 <senthurnethra123@gmail.com>
2025-01-18 16:23:54 +0530

    Python practice

--- main.py
+++ main.py
@@ -10,13 +10,52 @@ from operator import truediv
 #print(f"goat {goat}")
 #price = 12.44
 
+#
+# print(f"per cake ${price}")
+#
+# is_good = True
+#
+# if is_good:
+#     print(f"ok to eat")
+# else:
+#     print(f"dont eat")
 
-#print(f"per cake ${price}")
+# num = 123
+# name = "Senthur"
+# decimal = 12.23
+# good = True
+# age = 25
 
-is_good = True
+# print(type(decimal))
 
-if is_good:
-    print(f"ok to eat")
-else:
-    print(f"dont eat")
+# num = int(num)
+# num = int(num)
 
+#TODO: Reinilization
+
+# name = float(num)
+# name += 1
+# print(name)
+
+# name = input("Name?: ")
+# age = int(input("Age?: "))
+# age += 1
+# print (f"hello {name}!")
+# print (f"you are {age}")
+
+# item = input("Name of the item: ")
+# quantity = int(input("no of item: "))
+# price = float(input("price of item: "))
+# total = price * quantity
+# print(total)
+# print(f"you have bought {quantity} x {item}/s")
+# print(f"for ${total}")
+
+A = input("enter an object: ")
+B = input("enter a verb: ")
+C = input("enter a verb: ")
+D = input("enter a noun: ")
+
+print(f"Life sometimes throws {A} at your door step")
+print(f"if you {B} that dont {C} it")
+print(f"it all was in the {D}")
