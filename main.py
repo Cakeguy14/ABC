d54966ca15c577cad08286943f979b3d74b2930f
Cakeguy14 <senthurnethra123@gmail.com>
2025-01-18 14:45:02 +0530

    Python practice

---
+++ main.py
@@ -0,0 +1,22 @@
+"""this is my first python program
+print("Hello World")
+print("Im your new king")
+"""
+from operator import truediv
+
+#Greatness = "senthur"
+#goat = "you are peak"
+#print(f"hello {Greatness}")
+#print(f"goat {goat}")
+#price = 12.44
+
+
+#print(f"per cake ${price}")
+
+is_good = True
+
+if is_good:
+    print(f"ok to eat")
+else:
+    print(f"dont eat")
+
